badcb1ef7933cf05aea5d8fa814f1412247be33e
Chrisu <c.bartsch@gmx.at>
2018-01-05 19:33:19 +0100

    Show level number in UI

---
+++ Assets/Scripts/Managers/UiManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiManager : MonoBehaviour
+{
+
+    public Image worldNum, levelNum1, levelNum2;
+
+    public List<Sprite> digitSprites;
+    
+	void Start () {
+		
+	}
+	
+	void Update ()
+	{
+        //update level numbers
+	    worldNum.sprite = digitSprites[GameManager.Instance.WorldNum % 10];
+	    levelNum1.sprite = digitSprites[(GameManager.Instance.LevelNum / 10) % 10];
+	    levelNum2.sprite = digitSprites[GameManager.Instance.LevelNum % 10];
+    }
+}
