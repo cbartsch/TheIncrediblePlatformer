ad6f2cf3b689cd158dabd9ea0d24631a85ff3f1c
Chrisu <c.bartsch@gmx.at>
2018-01-08 17:52:19 +0100

    Add Amplitude SDK + analytics manager

---
+++ Assets/Scripts/Managers/GameAnalytics.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class GameAnalytics : MonoBehaviour
+{
+    public static GameAnalytics Instance { get; private set; }
+
+    void Start()
+    {
+        Instance = this;
+
+        Amplitude amplitude = Amplitude.Instance;
+        amplitude.logging = true;
+        amplitude.init("451e76614d15535b86777ebd2bf87125");
+
+        Amplitude.Instance.logEvent("StartGame");
+
+        Analytics.enabled = true;
+        Analytics.deviceStatsEnabled = true;
+    }
+    
+    void Update()
+    {
+
+    }
+
+    public void LevelFinished(int worldNum, int levelNum, TimeSpan timeDiff)
+    {
+        LogEvent("LevelFinished", new Dictionary<string, object>
+        {
+            {"WorldNum", worldNum},
+            {"LevelNum", levelNum},
+            {"Time", timeDiff.TotalMilliseconds}
+        });
+    }
+
+    private void LogEvent(string eventName, Dictionary<string, object> eventParams)
+    {
+        Debug.Log("[Analytics] Log event " + eventName);
+        Analytics.CustomEvent(eventName, eventParams);
+        Amplitude.Instance.logEvent(eventName, eventParams);
+    }
+}
