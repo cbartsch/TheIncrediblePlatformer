eeb70b86376d6bac23776b8187a53feeadb3a756
Chrisu <c.bartsch@gmx.at>
2018-02-09 02:15:37 +0100

    Remove JS google analytics and call from unity only

--- Assets/Scripts/Managers/GameAnalytics.cs
+++ Assets/Scripts/Managers/GameAnalytics.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using UnityEngine;
 using UnityEngine.Analytics;
 
@@ -11,27 +9,24 @@ public class GameAnalytics : MonoBehaviour
 
     public GoogleAnalyticsV4 ga;
 
-#if UNITY_WEBGL
-    [DllImport("__Internal")]
-    private static extern void JS_LevelFinished(int worldIndex, int levelIndex);
-#endif
-
     void Start()
     {
         Instance = this;
 
+#if UNITY_EDITOR
+        return;
+#endif
         Amplitude amplitude = Amplitude.Instance;
         amplitude.logging = true;
         amplitude.init("451e76614d15535b86777ebd2bf87125");
 
-        LogEvent("StartGame", null);
-
         Analytics.enabled = true;
         Analytics.deviceStatsEnabled = true;
 
+        LogEvent("StartGame", null);
         ga.LogEvent("general", "StartGame", "", 0);
     }
-    
+
     void Update()
     {
 
@@ -39,6 +34,10 @@ public class GameAnalytics : MonoBehaviour
 
     public void LevelFinished(int worldNum, int levelNum, TimeSpan timeDiff)
     {
+#if UNITY_EDITOR
+        return;
+#endif
+
         LogEvent("LevelFinished", new Dictionary<string, object>
         {
             {"WorldIndex", worldNum + 1},
@@ -47,14 +46,14 @@ public class GameAnalytics : MonoBehaviour
         });
 
         ga.LogEvent("general", "LevelFinished_" + (worldNum + 1) + "_" + (levelNum + 1), "", (long)timeDiff.TotalMilliseconds);
-
-#if UNITY_WEBGL && !UNITY_EDITOR
-        JS_LevelFinished(worldNum + 1, levelNum + 1);
-#endif
     }
 
     private void LogEvent(string eventName, Dictionary<string, object> eventParams)
     {
+#if UNITY_EDITOR
+        return;
+#endif
+
         Debug.Log("[Analytics] Log event " + eventName);
         Analytics.CustomEvent(eventName, eventParams);
         Amplitude.Instance.logEvent(eventName, eventParams);
